fb9dfe49c21d531b2e01ac230dcb4065a81d62b6
Valentamayog <vatamayog@unal.edu.co>
2025-05-10 09:17:41 -0500

    Creación del test_parse_args

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,1 +1,32 @@
+import argparse
 
+
+def parse_args():
+
+    parser = argparse.ArgumentParser(description="Count words in files.")
+
+    parser.add_argument(
+        "input",
+        type=str,
+        help="Path to the input folder containing files to process",
+    )
+    parser.add_argument(
+        "output",
+        type=str,
+        help="Path to the output folder for results",
+    )
+
+    parsed_args = parser.parse_args()
+
+    return parsed_args.input, parsed_args.output
+
+
+def main():
+    input_folder, output_folder = parse_args()
+
+    print("input foder:", input_folder)
+    print("output folder:", output_folder)
+
+
+if __name__ == "__main__":
+    main()
